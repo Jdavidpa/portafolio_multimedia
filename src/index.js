2e7e0d6eee59ed5179e54a7ab7e9968ab3427397
Jdavidpa <bejopalo2@gmail.com>
2019-05-16 20:54:11 +0000

    datos de modelo

--- src/index.js
+++ src/index.js
@@ -7,10 +7,34 @@ app.get("/", (req, res) => {
 
 app.get("/categorias", (req, res) => {
   let categorias = [
-    { name: "C.naturales", descripcion: "ecosistema" },
-    { name: "Matematicas", descripcion: "", age: 32 }
+    { idCategoria: "10", name: "C.naturales", descripcion: "ecosistema" },
+    { idCategoria: "20", name: "Matematicas", descripcion: "polinomios" }
   ];
-  res.send(users);
+  res.send(categorias);
+});
+
+app.get("/usuarios", (req, res) => {
+  let usuarios = [
+    { idUsuario: "001", name: "Pedro", apellido: "Martinez" , email: "martinemar@gmail.com" , telefono: "3234531232" , contreaseña: "32345", idGrdao: "2"},
+    { idUsuario: "002", name: "Ana", apellido: "Gomez" , email: "anag12@gmail.com" , telefono: "3233231132" , contreaseña: "14545", idGrado: "1"},
+  ];
+  res.send(usuarios);
+});
+
+app.get("/producto_multimedia", (req, res) => {
+  let producto_multimedia = [
+    { name: "infografia", descripcion: "infografia sobre ecosistema" , fecha_creacion: "15/05/2019" , idUsuario: "001" , idCategoria: "10"},
+    { name: "video", descripcion: "video sobre polinomios" , fecha_creacion: "12/04/2019" , idUsuario: "002" , idCategoria: "20"},
+  ];
+  res.send(producto_multimedia);
+});
+
+app.get("/grado", (req, res) => {
+  let grado = [
+    { idGrado: "1", name: "9°", descripcion: "el grado consta de 15 mujeres y 12 varones" },
+    { idGrado: "2", name: "10°", adescripcion: "el grado consta de 12 mujeres y 20 varones" },
+  ];
+  res.send(grado);
 });
 app.listen(3000, () => {
   console.log("Server on port http://localhost:3000");
